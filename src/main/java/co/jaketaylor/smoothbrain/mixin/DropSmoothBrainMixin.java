3f893305c803218a3289a367fd663ba4147e0902
Jake Taylor <me@jaketaylor.co>
2019-08-23 16:36:39 +0100

    even smoother

--- src/main/java/co/jaketaylor/smoothbrain/mixin/DropSmoothBrainMixin.java
+++ src/main/java/co/jaketaylor/smoothbrain/mixin/DropSmoothBrainMixin.java
@@ -1,9 +1,6 @@
 package co.jaketaylor.smoothbrain.mixin;
 
-import java.util.UUID;
-
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
